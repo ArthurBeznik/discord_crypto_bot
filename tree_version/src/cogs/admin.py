3ccd81418be97ac3388b16e67e1ef29610335d6b
Arthur Beznik <arthurbeznik@gmail.com>
2024-09-09 17:50:39 +0200

    I need to clean

---
+++ tree_version/src/cogs/admin.py
@@ -0,0 +1,86 @@
+import discord
+import os
+from discord.ext import commands
+from discord import app_commands
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+GUILD_ID = os.getenv('DISCORD_GUILD_ID')
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+MY_GUILD = discord.Object(id=GUILD_ID)
+
+logger = logging.getLogger(__name__)
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="load", description="Load a new cog")
+    @app_commands.rename(cog_name='cog')
+    @app_commands.describe(cog_name='Name of the cog to load')
+    async def load(self, interaction: discord.Interaction, cog_name: str):
+        """
+        Slash command to load a new cog.
+        """
+        try:
+            await self.bot.load_extension(f"cogs.{cog_name}")
+            await interaction.response.send_message(f"Loaded extension **{cog_name}.py**")
+            logger.info(f"Loaded cog: {cog_name}")
+        except Exception as e:
+            await interaction.response.send_message(f"Error loading cog: {e}")
+            logger.error(f"Failed to load cog: {cog_name}")
+
+    @app_commands.command(name="sync", description="Sync commands to the Discord server")
+    async def sync(self, interaction: discord.Interaction):
+        """
+        Slash command to sync commands.
+        """
+        try:
+            await self.bot.tree.sync(guild=MY_GUILD)
+            logger.info(self.bot.tree.get_commands(guild=MY_GUILD))
+            await interaction.response.send_message(f"Commands synced to guild {MY_GUILD.id}")
+            logger.info(f"Synced commands to guild: {MY_GUILD}")
+        except Exception as e:
+            await interaction.response.send_message(f"Error syncing commands: {e}")
+            logger.error(f"Failed to sync commands: {e}")
+
+    @app_commands.command(name="remove", description="Remove a specific command from the Discord server")
+    @app_commands.rename(command_name='command')
+    @app_commands.describe(command_name='Name of the command to remove')
+    async def remove(self, interaction: discord.Interaction, command_name: str):
+        """
+        Slash command to remove a command.
+        """
+        try:
+            logger.info('Remove 0', self.bot.tree.get_commands(guild=MY_GUILD))
+            # self.bot.tree.remove_command(command_name)
+            # self.bot.remove_command(command_name)
+            await self.bot.tree.sync(guild=MY_GUILD)
+            logger.info('Remove 1', self.bot.tree.get_commands(guild=MY_GUILD))
+            await interaction.response.send_message(f"Commands synced to guild {MY_GUILD.id}")
+            logger.info(f"Removed command {command_name} from guild: {MY_GUILD}")
+        except Exception as e:
+            await interaction.response.send_message(f"Error syncing commands: {e}")
+            logger.error(f"Failed to remove command {command_name}: {e}")
+
+    @app_commands.command(name="clear", description="Clear all commands from the server")
+    async def clear(self, interaction: discord.Interaction):
+        """
+        Slash command to clear all commands.
+        """
+        try:
+            logger.info(self.bot.tree.get_commands(guild=MY_GUILD))
+            self.bot.tree.clear_commands(guild=MY_GUILD)
+            await self.bot.tree.sync(guild=MY_GUILD)
+            logger.info(self.bot.tree.get_commands(guild=MY_GUILD))
+            logger.info(self.bot.tree.get_commands(guild=MY_GUILD))
+            await interaction.response.send_message(f"Commands synced to guild {MY_GUILD.id}")
+            logger.info(f"Cleared all commands from guild: {MY_GUILD}")
+        except Exception as e:
+            await interaction.response.send_message(f"Error syncing commands: {e}")
+            logger.error(f"Failed to clear commands: {e}")
+
+
+async def setup(bot):
+    await bot.add_cog(Admin(bot))
---
+++ tree_version/src/main.py
@@ -0,0 +1,108 @@
+# main.py
+
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    # level=logging.DEBUG,
+    format='%(asctime)s [%(levelname)s] %(name)s: %(message)s', datefmt='%H:%M:%S', # without date
+    handlers=[
+        logging.FileHandler("bot.log"),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+GUILD_ID = os.getenv('DISCORD_GUILD_ID')
+BOT_TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+MY_GUILD = discord.Object(id=GUILD_ID)
+
+# intents = discord.Intents.default()
+intents=discord.Intents(
+        # kwargs found at https://docs.pycord.dev/en/master/api.html?highlight=discord%20intents#discord.Intents
+        guilds=True, members=True, messages=True, reactions=True,
+        presences=True, message_content=True,
+    )
+
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+async def inspect_cog(cog_name: str):
+    cog = bot.get_cog(cog_name)
+    commands = cog.get_app_commands()
+    logger.info([c.name for c in commands])
+
+async def load_cogs():
+    """
+    Dynamically loads all cogs from the `cogs` folder.
+    """
+    for filename in os.listdir("tree_version/src/cogs"):
+        if filename.endswith(".py"):
+            try:
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                logger.info(f"Loaded extension: {filename}")
+            except Exception as e:
+                logger.error(f"Failed to load extension {filename}: {e}")
+
+async def unload_cogs():
+    """
+    Dynamically loads all cogs from the `cogs` folder.
+    """
+    for filename in os.listdir("tree_version/src/cogs"):
+        if filename.endswith(".py"):
+            try:
+                await bot.unload_extension(f"cogs.{filename[:-3]}")
+                logger.info(f"Unloaded extension: {filename}")
+            except Exception as e:
+                logger.error(f"Failed to unload extension {filename}: {e}")
+
+async def remove_cogs():
+    for filename in os.listdir("tree_version/src/cogs"):
+        if filename.endswith(".py"):
+            try:
+                await bot.remove_cog(f"cogs.{filename[:-3]}")
+                logger.info(f"Removed extension: {filename}")
+            except Exception as e:
+                logger.error(f"Failed to remove extension {filename}: {e}")
+
+async def clear_commands():
+    bot.tree.clear_commands(guild=MY_GUILD)
+    bot.recursively_remove_all_commands()
+    await bot.tree.sync(guild=MY_GUILD)
+
+async def sync_tree():
+    bot.tree.copy_global_to(guild=MY_GUILD)
+    await bot.tree.sync(guild=MY_GUILD)
+
+@bot.event
+async def on_ready():
+    # # Load cogs and sync tree
+    # await load_cogs()
+    # bot.tree.copy_global_to(guild=MY_GUILD)
+    # await bot.tree.sync(guild=MY_GUILD)
+    # logger.info(bot.tree.get_commands(guild=MY_GUILD))
+
+    # # Clear commands, remove cogs, unload cogs and sync tree
+    # await clear_commands()
+    # await remove_cogs()
+    # await unload_cogs()
+    # bot.tree.copy_global_to(guild=MY_GUILD)
+    # await bot.tree.sync(guild=MY_GUILD)
+    # logger.info(bot.tree.get_commands(guild=MY_GUILD))
+
+    # Reload cogs and sync tree
+    await load_cogs()
+    await sync_tree()
+    logger.info(bot.tree.get_commands(guild=MY_GUILD))
+    await inspect_cog('Misc')
+
+    # Bot is ready
+    logger.info(f"Logged in as {bot.user}")
+
+
+bot.run(BOT_TOKEN)
